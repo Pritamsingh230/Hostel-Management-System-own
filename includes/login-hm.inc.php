1e9963c3aac137b3c2eed818a140558bd978520b
Pritamsingh230 <146520739+Pritamsingh230@users.noreply.github.com>
2024-09-17 13:17:55 +0530

    Update login-hm.inc.php

--- includes/login-hm.inc.php
+++ includes/login-hm.inc.php
@@ -1,67 +1,43 @@
 <?php
-
 if (isset($_POST['login-submit'])) {
 
-  require 'config.inc.php';
+    require 'config.inc.php';
 
-  $username = $_POST['username'];
-  $password = $_POST['pwd'];
+    // Retrieve and sanitize input data
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = $_POST['pwd'];
 
-  if (empty($username) || empty($password)) {
-    header("Location: ../login-hostel_manager.php?error=emptyfields");
-    exit();
-  }
-  else {
-    $sql = "SELECT *FROM Hostel_Manager WHERE Username = '$username'";
-    $result = mysqli_query($conn, $sql);
-    if($row = mysqli_fetch_assoc($result)){
-      $pwdCheck = password_verify($password, $row['Pwd']);
-      if($pwdCheck == false){
-        header("Location: ../login-hostel_manager.php?error=wrongpwd");
+    // Check if inputs are empty
+    if (empty($username) || empty($password)) {
+        header("Location: ../login-hostel_manager.php?error=emptyfields");
         exit();
-      }
-      else if($pwdCheck == true) {
+    } else {
+        // Prepare and execute SQL query
+        $sql = "SELECT * FROM Hostel_Manager WHERE Username = ?";
+        $stmt = mysqli_prepare($conn, $sql);
 
-        //session_start();
-        $_SESSION['hostel_man_id'] = $row['Hostel_man_id'];
-        $_SESSION['fname'] = $row['Fname'];
-        $_SESSION['lname'] = $row['Lname'];
-        $_SESSION['mob_no'] = $row['Mob_no'];
-        $_SESSION['username'] = $row['Username'];
-        $_SESSION['hostel_id'] = $row['Hostel_id'];
-        $_SESSION['email'] = $row['Email'];
-        $_SESSION['isadmin'] = $row['Isadmin'];
-        $_SESSION['PSWD'] = $row['Pwd'];
+        if (!$stmt) {
+            header("Location: ../login-hostel_manager.php?error=sqlerror");
+            exit();
+        }
 
-        //Just for checking if session variables are working properly
-        if(isset($_SESSION['username'])){
-          echo "<script type='text/javascript'>alert('Set')</script>";
-        }
-        else {
-          echo "<script type='text/javascript'>alert('Not SET')</script>";
-        }
-        //echo $_SESSION['lname'];
-        if($_SESSION['isadmin']==0){
-          header("Location: ../home_manager.php?login=success");
-        }
-        else {
-          header("Location: ../admin/admin_home.php?login=success");
-        }
-        //exit();
-      }
-      else {
-        header("Location: ../login-hostel_manager.php?error=strangeerr");
-        exit();
-      }
-    }
-    else{
-      header("Location: ../login-hostel_manager.php?error=nouser");
-      exit();
-    }
-  }
+        mysqli_stmt_bind_param($stmt, "s", $username);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
 
-}
-else {
-  header("Location: ../login-hostel_manager.php");
-  exit();
-}
+        if ($row = mysqli_fetch_assoc($result)) {
+            // Verify password
+            if (password_verify($password, $row['Pwd'])) {
+                // Start session and set session variables
+                session_start();
+                $_SESSION['hostel_man_id'] = $row['Hostel_man_id'];
+                $_SESSION['fname'] = $row['Fname'];
+                $_SESSION['lname'] = $row['Lname'];
+                $_SESSION['mob_no'] = $row['Mob_no'];
+                $_SESSION['username'] = $row['Username'];
+                $_SESSION['hostel_id'] = $row['Hostel_id'];
+                $_SESSION['email'] = $row['Email'];
+                $_SESSION['isadmin'] = $row['Isadmin'];
+
+                // Redirect based on user role
+                if ($_SESSION['isadmin'] =
