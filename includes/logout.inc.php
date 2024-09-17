38b669c5dafa1c0cd036c277ff943e18fa24a32a
Pritamsingh230 <146520739+Pritamsingh230@users.noreply.github.com>
2024-09-17 13:21:11 +0530

    Update logout.inc.php

--- includes/logout.inc.php
+++ includes/logout.inc.php
@@ -1,8 +1,11 @@
 <?php
 
-  session_start();
-  session_unset();
-  session_destroy();
-  header("Location: ../index.php")
+session_start();
+session_unset(); // Clear all session variables
+session_destroy(); // Destroy the session
 
- ?>
+// Redirect to the homepage
+header("Location: ../index.php");
+exit(); // Ensure that no further code is executed
+
+?>
