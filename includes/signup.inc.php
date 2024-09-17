b1f99364fee9bd76443132e0f77ccd4c24eb5b71
Pritamsingh230 <146520739+Pritamsingh230@users.noreply.github.com>
2024-09-17 13:30:56 +0530

    Update signup.inc.php

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -22,38 +22,41 @@ if (isset($_POST['signup-submit'])) {
         header("Location: ../signup.php?error=passwordcheck");
         exit();
     } else {
-        // Check if user already exists
+        // Prepare and execute SQL query to check if user already exists
         $sql = "SELECT Student_id FROM Student WHERE Student_id=?";
-        $stmt = mysqli_stmt_init($conn);
-        if (!mysqli_stmt_prepare($stmt, $sql)) {
+        $stmt = mysqli_prepare($conn, $sql);
+        if (!$stmt) {
             header("Location: ../signup.php?error=sqlerror");
             exit();
-        } else {
-            mysqli_stmt_bind_param($stmt, "s", $roll);
-            mysqli_stmt_execute($stmt);
-            mysqli_stmt_store_result($stmt);
-            $resultCheck = mysqli_stmt_num_rows($stmt);
-            if ($resultCheck > 0) {
-                header("Location: ../signup.php?error=userexists");
-                exit();
-            } else {
-                // Insert new user
-                $sql = "INSERT INTO Student (Student_id, Fname, Lname, Mob_no, Dept, Year_of_study, Pwd) VALUES (?, ?, ?, ?, ?, ?, ?)";
-                if (!mysqli_stmt_prepare($stmt, $sql)) {
-                    header("Location: ../signup.php?error=sqlerror");
-                    exit();
-                } else {
-                    $hashedPwd = password_hash($password, PASSWORD_DEFAULT);
-                    mysqli_stmt_bind_param($stmt, "sssssss", $roll, $fname, $lname, $mobile, $dept, $year, $hashedPwd);
-                    mysqli_stmt_execute($stmt);
-                    header("Location: ../index.php?signup=success");
-                    exit();
-                }
-            }
         }
+        
+        mysqli_stmt_bind_param($stmt, "s", $roll);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_store_result($stmt);
+        $resultCheck = mysqli_stmt_num_rows($stmt);
+        
+        if ($resultCheck > 0) {
+            header("Location: ../signup.php?error=userexists");
+            exit();
+        } else {
+            // Prepare and execute SQL query to insert new user
+            $sql = "INSERT INTO Student (Student_id, Fname, Lname, Mob_no, Dept, Year_of_study, Pwd) VALUES (?, ?, ?, ?, ?, ?, ?)";
+            $stmt = mysqli_prepare($conn, $sql);
+            if (!$stmt) {
+                header("Location: ../signup.php?error=sqlerror");
+                exit();
+            }
+            
+            $hashedPwd = password_hash($password, PASSWORD_DEFAULT);
+            mysqli_stmt_bind_param($stmt, "sssssss", $roll, $fname, $lname, $mobile, $dept, $year, $hashedPwd);
+            mysqli_stmt_execute($stmt);
+            header("Location: ../index.php?signup=success");
+            exit();
+        }
+        
+        mysqli_stmt_close($stmt);
     }
 
-    mysqli_stmt_close($stmt);
     mysqli_close($conn);
 
 } else {
